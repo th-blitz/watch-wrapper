cf906c17e8636d2694b8d21909fbc9a6365029c4
th-blitz <PreethamRakshith11@gmail.com>
2024-05-17 17:43:30 -0700

    check_interval()

--- watch.sh
+++ watch.sh
@@ -6,4 +6,24 @@ function print() {
 	printf "%s\n" " "
 }
 
+function check_interval() {
+
+	watch_interval=$1
+	MIN_VALUE=$2
+
+	# allow numbers : +n, -n, .n, +.n, -.n, +., -., ., +, - etc;
+	if [[ $watch_interval =~ ^[-+]?(([0-9]*)?(\.)?([0-9]*)?)$ ]]; then
+		watch_interval=${watch_interval#+}
+		# replace + or - with 0;
+		if [[ $watch_interval =~ [-+]? ]]; then 
+			watch_interval=0
+		fi
+		if [ $( echo "$watch_interval < 1" | bc -l ) -eq 1 ]; then
+			print "less than 1"
+			return 1
+		fi
+	fi
+	return 0
+}
+
 
